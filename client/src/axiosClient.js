1e9154db57de90cb8551fda593e1a2d395e1f06a
KavinNaresh <kavinnaresh97@gmail.com>
2024-12-02 12:51:04 +0530

    Update axiosClient.js

--- client/src/axiosClient.js
+++ client/src/axiosClient.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 
 const axiosClient = axios.create({
-  baseURL : 'http://localhost:5000/',
+  baseURL : ' process.env.REACT_APP_API_URL',
   headers:{
     'Accept': 'application/json',
     'Content-Type': 'application/json',
